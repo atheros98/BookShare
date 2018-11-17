e262b2dc8cb165bf7b407fe26fc086e51aea7f63
MinhNV <nguyenvuminh98@gmail.com>
2018-11-17 10:57:41 +0700

    Fix sql query on GetRowCount method

--- DAL/BookDAO.cs
+++ DAL/BookDAO.cs
@@ -179,16 +179,16 @@ namespace DAL
                     sqlCommand = "select count (distinct id) from"
                             + "(select * from Book where title LIKE '%" + queryStr + "%'"
                     + " union select * from Book where ISBN LIKE '%" + queryStr + "%'"
-                    + " union select * from Book where author LIKE '%" + queryStr + "%') result where status = " + Book.STATUS_ACCEPTED;
+                    + " union select * from Book where author LIKE '%" + queryStr + "%') result and status = " + Book.STATUS_ACCEPTED;
                     break;
                 case "Title":
-                    sqlCommand = "select count (*) from Book where title LIKE '%" + queryStr + "%' where status = " + Book.STATUS_ACCEPTED; 
+                    sqlCommand = "select count (*) from Book where title LIKE '%" + queryStr + "%' and status = " + Book.STATUS_ACCEPTED; 
                     break;
                 case "Author":
-                    sqlCommand = "select count (*) from Book where author LIKE '%" + queryStr + "%' where status = " + Book.STATUS_ACCEPTED; 
+                    sqlCommand = "select count (*) from Book where author LIKE '%" + queryStr + "%' and status = " + Book.STATUS_ACCEPTED; 
                     break;
                 case "ISBN":
-                    sqlCommand = "select count (*) from Book where ISBN LIKE '%" + queryStr + "%' where status = " + Book.STATUS_ACCEPTED; 
+                    sqlCommand = "select count (*) from Book where ISBN LIKE '%" + queryStr + "%' and status = " + Book.STATUS_ACCEPTED; 
                     break;
             }
 
